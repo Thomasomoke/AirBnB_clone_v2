8adef0db7268a54df91b8a38e21cef3f18e3cc10
Thomas Omoke <omokethomas644@gmail.com>
2024-06-26 14:26:19 -0700

    change

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,10 +9,12 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage.
 
@@ -36,7 +38,6 @@ class FileStorage:
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
-
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
@@ -47,8 +48,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
-
+        """Loads storage dictionary from file."""
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -71,7 +71,7 @@ class FileStorage:
            the method should not do anything
         """
         if obj is None:
-                return
+            return
         obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
 
         try:
